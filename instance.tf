08eb4f0b04fa85fe38581445085217a1ea00fe0b
Kabarukhin Aleksei <aleksei.kabarukhin@nxlog.org>
2022-11-22 04:26:01 +0300

    first commit

---
+++ instance.tf
@@ -0,0 +1,62 @@
+resource "aws_instance" "db2-server-1" {
+  ami           = data.aws_ami.rhel_8_7.id
+  instance_type = "t3.medium"
+  # VPC
+  subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+  # Security Group
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  # the Public SSH key
+  key_name = "${aws_key_pair.us-west2-key-pair.id}"
+  connection {
+      user = "${var.EC2_USER}"
+      private_key = "${file("${var.PRIVATE_KEY_PATH}")}"
+  }
+
+  tags = {
+    Name = "db2-server-1"
+  }
+}
+
+resource "aws_instance" "db2-server-2" {
+  ami           = data.aws_ami.rhel_8_7.id
+  instance_type = "t3.medium"
+  # VPC
+  subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+  # Security Group
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  # the Public SSH key
+  key_name = "${aws_key_pair.us-west2-key-pair.id}"
+  connection {
+      user = "${var.EC2_USER}"
+      private_key = "${file("${var.PRIVATE_KEY_PATH}")}"
+  }
+
+  tags = {
+    Name = "db2-server-1"
+  }
+}
+
+resource "aws_instance" "app-server" {
+  ami           = data.aws_ami.rhel_8_7.id
+  instance_type = "t3.small"
+  # VPC
+  subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+  # Security Group
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  # the Public SSH key
+  key_name = "${aws_key_pair.us-west2-key-pair.id}"
+  connection {
+      user = "${var.EC2_USER}"
+      private_key = "${file("${var.PRIVATE_KEY_PATH}")}"
+  }
+
+  tags = {
+    Name = "app-server"
+  }
+}
+
+// Sends your public key to the instance
+resource "aws_key_pair" "us-west2-key-pair" {
+    key_name = "us-west2-key-pair"
+    public_key = "${file(var.PUBLIC_KEY_PATH)}"
+}
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+
+variable "AWS_AVAILABILITY_ZONE" {
+  default = "us-west-2a"
+}
+
+variable "PUBLIC_KEY_PATH" {
+    default = "./us-west2-key-pair.pub"
+}
